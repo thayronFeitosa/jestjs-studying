ad3e1a1ed8328d4f43ceb4bc2c573e397b7cff92
thayron matos <thayronf3@gmail.com>
2022-12-23 21:39:41 -0300

    adds dependecy mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://mongo:27017/clean-node-api'),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
